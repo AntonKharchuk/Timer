270859bd48f3b036ef2d7a5d850e983ddacf2141
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-31 14:55:04 +0200

    add console Digit and first set of digits

---
+++ Timer.ConsoleDemo/ConsoleDigit.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Timer.ConsoleDemo
+{
+    public class ConsoleDigit
+    {
+        public string[] Digit;
+
+        public void WriteDigitToConsole(int startX, int startY)
+        {
+            for (int i = 0; i < Digit.Length; i++)
+            {
+                Console.CursorLeft = startX;
+                Console.CursorTop = startY + i;
+                Console.Write(Digit[i]);
+            }
+        }
+    }
+}
---
+++ Timer.ConsoleDemo/ConsoleDigitSets.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Timer.ConsoleDemo
+{
+    public class ConsoleDigitSets
+    {
+        public ConsoleDigit[] Digits5x5 = new ConsoleDigit[]
+       {
+            new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "#   #",
+                    "#   #",
+                    "#   #",
+                    "#####"
+                }
+            },
+             new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "    #",
+                    "    #",
+                    "    #",
+                    "    #",
+                    "    #"
+                }
+            },
+              new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "    #",
+                    "#####",
+                    "#    ",
+                    "#####"
+                }
+            },
+               new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "    #",
+                    "#####",
+                    "    #",
+                    "#####"
+                }
+            },
+                new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#   #",
+                    "#   #",
+                    "#####",
+                    "    #",
+                    "    #"
+                }
+            },
+                 new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "#    ",
+                    "#####",
+                    "    #",
+                    "#####"
+                }
+            },
+                  new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "#    ",
+                    "#####",
+                    "#   #",
+                    "#####"
+                }
+            },
+                   new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "    #",
+                    "    #",
+                    "    #",
+                    "    #"
+                }
+            },
+                    new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "#   #",
+                    "#####",
+                    "#   #",
+                    "#####"
+                }
+            },
+                    new ConsoleDigit()
+            {
+                Digit = new string[]
+                {
+                    "#####",
+                    "#   #",
+                    "#####",
+                    "    #",
+                    "#####"
+                }
+            },
+       };
+    }
+}
